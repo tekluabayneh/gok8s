a2a3a11327f78a99e79b08e4e39ca74dfe61eb51
teklu <tekluabayneh@gmail.com>
2026-06-25 17:46:29 +0300

    feat: update kubectl server

--- Kubectl/server.ts
+++ Kubectl/server.ts
@@ -1,9 +1,19 @@
 import e from "express"
 import express from "express"
+import { load, loadAll } from 'js-yaml'
+import { readFileSync } from 'node:fs'
 const app = express()
-
 app.get("/", (req, res) => {
   console.log("test")
+  try {
+    let val = load(readFileSync("./file.yaml", 'utf8'))
+    console.log(val)
+    res.send(val)
+  } catch (error) {
+    res.send(error)
+    console.log(error)
+  }
+
 })
 
 app.listen(8000, (error) => {
