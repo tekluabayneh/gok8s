7650a2868af3c13018e9f3c0ddc1d0a820efa7ba
teklu <tekluabayneh@gmail.com>
2026-06-25 17:46:30 +0300

    feat: update node handler

--- cluster/cmd/apiServer/handlers/node.go
+++ cluster/cmd/apiServer/handlers/node.go
@@ -1,1 +1,38 @@
 package handlers
+
+import (
+	"context"
+	"net/http"
+)
+
+type NodeStore interface {
+	GetNode(ctx context.Context, name string)
+	RegisterNode(ctx context.Context, name string, value string)
+}
+
+type NodeHandler struct {
+	Store NodeStore
+}
+
+// hard coded configuration
+type Default struct {
+	Controleplane string
+}
+
+//// TODO: POST /api/v1/nodes
+//   - parse node info from request body
+//   - validate (name can't be empty)
+//   - write to etcd at key /registry/nodes/{name}
+//   - return 201
+
+// TODO: GET /api/v1/nodes
+//   - read all keys under /registry/nodes/
+//   - return as JSON array
+
+func (p *NodeHandler) GetNodeHandler(w http.ResponseWriter, r *http.Request) {
+	p.Store.GetNode(r.Context(), "fakename")
+}
+
+func (p *NodeHandler) RegisterNodeHandler(w http.ResponseWriter, r *http.Request) {
+	p.Store.GetNode(r.Context(), "fakename")
+}
