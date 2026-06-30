f68259d7a7f4b50c3260b8dc0c6b7fddb996a668
teklu <tekluabayneh@gmail.com>
2026-06-30 11:53:12 +0300

    update toPod func for creating proper service and incomming body match with type

--- cluster/internals/mapper/pod.go
+++ cluster/internals/mapper/pod.go
@@ -2,6 +2,8 @@ package mapper
 
 import "github.com/tekluabayneh/gok8s/config"
 
+// TODO
+// pod configation  need to be change as is it only contain few of configuration for testing
 func ToPod(conf config.Pod) config.Pod {
 	pod := config.Pod{
 		APIVersion: conf.Kind,
