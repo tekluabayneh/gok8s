f825c1585a590f3ac53b6813216dc57c0c2afe65
teklu <tekluabayneh@gmail.com>
2026-08-23 17:52:03 +0300

    refactor: adopt oclif for kubectl command handling

---
+++ Kubectl/src/commands/get/pods.ts
@@ -0,0 +1,11 @@
+import { Command, Flags } from '@oclif/core'
+
+export default class pods extends Command {
+  static flags = {
+    from: Flags.string({ char: 'f', description: 'Who is saying hello', required: true }),
+  }
+
+  async run(): Promise<void> {
+    this.log(`well this work i get pood is been fetched...`)
+  }
+}
