45763176a55bac9d26623ee088a3ea31a03ff673
teklu <tekluabayneh@gmail.com>
2026-09-02 00:51:33 +0300

    fix(kubectl): resolve auth via context's user/cluster, add TLS + client-cert support

--- Kubectl/src/client/client.ts
+++ Kubectl/src/client/client.ts
@@ -1,78 +1,108 @@
 import axios from "axios"
-import fs from "fs"
 import os from "os"
 import { yamlToJson } from "../utils/yamlToJson.js"
 import chalk from "chalk"
-import { parse } from "yaml"
+import KubectlconfigType from "../../types/configtypes.d.js"
+import { promises as stPromise } from "fs"
+import https from "https"
 
-//FIRE: 
-//one must thing i have to taken care of is all resouce does not need same 
-//RBAC so things change and that also need to be taken care of
-//HOT: 
-// 
-//WARM: 
-//
-//COOL: 
-//
-//ICE: 
-//
-//DONE: reading and extracting yaml file to json already is done so i could use that with some modificatin to the function 
+let currentContext = ""
+let certificateAuthorityData = ""
+let insecureSkipTlsVerify = false
+let clientCertificateData = ""
+let clientKeyData = ""
 let BASE_URL = ""
-
-
-// get base url form mykbectl config 
-// TODO 
-// read conf file from ~/.kube/config this files will provide all the config that kubectl needs 
-const api = axios.create({ baseURL: BASE_URL, timeout: 20000, withCredentials: true })
-
-//TODO 
-//
-//read file from ~/.kube/config DONE:
-
-//make sure the directory and the files exist  DONE:
-//and make sure file is not empty  DONE:
-
-//make sure files has properate fiels that are need for apiserver 
-
-//make sure any error are handled well 
-// const RootPath = fs.readdir(, options, callback)
-
-
 let token = ""
-const defaultPath = os.homedir() + "/.kube/config"
-fs.stat(defaultPath, (error, stats) => {
-  if (error) {
+let contextUser = ""
+let contextCluster = ""
+
+async function LoadConfig() {
+  const defaultPath = os.homedir() + "/.kube/config"
+  let stats
+  try {
+    stats = await stPromise.stat(defaultPath)
+  } catch (error) {
     console.log(chalk.red(error))
+  }
+
+  if (stats && stats?.size < 1) {
+    console.log(chalk.red("file is empty"))
     return
   }
 
-  // change to json and extract out what needs to be send through the api  
-  console.log(stats.isFile())
-  if (stats.size > 0) {
-    const yaml = fs.readFileSync(defaultPath, "utf-8")
-    const jsonFile = parse(yaml)
+  const jsonFile = await yamlToJson<KubectlconfigType>(defaultPath)
+  if (!jsonFile) return
 
-    console.log("file form .kube", jsonFile)
+
+  currentContext = jsonFile["current-context"]
+
+  for (const ctx of jsonFile["contexts"]) {
+    if (ctx.name == currentContext) {
+      contextUser = ctx.context.user
+      contextCluster = ctx.context.cluster
+    }
   }
 
+  for (const key in jsonFile) {
+    if (key == "users") {
+      for (const usr of jsonFile[key]) {
+        if (usr.name == contextUser) {
+          token = usr?.user?.token ?? ""
+          clientCertificateData = usr.user["client-certificate-data"] ?? ""
+          clientKeyData = usr.user["client-key-data"] ?? ""
+        }
+      }
+    }
+
+    if (key == "clusters") {
+      for (const clus of jsonFile[key]) {
+        if (clus.name == contextCluster) {
+          BASE_URL = clus.cluster.server
+          certificateAuthorityData = clus.cluster["certificate-authority-data"] ?? ""
+          insecureSkipTlsVerify = clus.cluster["insecure-skip-tls-verify"] ?? false
+        }
+      }
+    }
+  }
+}
+
+await LoadConfig()
+async function buildAgent() {
+  const agentOpts: https.AgentOptions = {}
+
+  if (insecureSkipTlsVerify) {
+    agentOpts.rejectUnauthorized = false
+  } else if (certificateAuthorityData) {
+    agentOpts.ca = Buffer.from(certificateAuthorityData, "base64")
+  }
+
+  if (clientCertificateData && clientKeyData) {
+    agentOpts.cert = Buffer.from(clientCertificateData, "base64")
+    agentOpts.key = Buffer.from(clientKeyData, "base64")
+  }
+
+  return new https.Agent(agentOpts)
+}
+
+const api = axios.create({
+  baseURL: BASE_URL,
+  timeout: 20000,
+  withCredentials: true,
+  httpsAgent: await buildAgent()
 })
 
 
 api.interceptors.request.use(async (config) => {
-  const publicRoutes = ["/fake one herer"];
+  const publicRoutes = ["/healthz", "/livez", "/readyz", "/version"]
 
 
   if (publicRoutes.some(route => config.url?.includes(route))) {
     return config;
   }
 
-
-  // we should be able to get token form config ~/.kube/config  file 
-
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
-
 
   return config;
 }, (error) => {
@@ -80,3 +110,4 @@ api.interceptors.request.use(async (config) => {
 });
 
 export default api
+
